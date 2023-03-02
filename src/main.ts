bede7d9bfc747c4e112481203521459c1406cad5
bartlian <lian_bart@163.com>
2023-03-02 11:39:34 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Vue, { defineComponent } from 'vue'
 const count = 1
 const App = defineComponent({
   render(h) {
+    // type VNodeChildren should support number type like vue3.x
     return h('div', count)
   }
 })
